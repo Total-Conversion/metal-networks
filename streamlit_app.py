8246b6e1f1466f000358eb2ef438d12704878f44
Tomasz Witosławski <34924114+Total-Conversion@users.noreply.github.com>
2024-11-14 17:15:49 +0000

    verify session storage is working

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,48 @@ from langchain_core.prompts import ChatPromptTemplate
 from texttable import Texttable
 import re
 
-st.title("🎈 My new Streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+models = st.selectbox(
+    "Models",
+    (
+        "Gemini 1.5 (itemization) + Claude 3.5 (inference)",
+        "GPT4 (itemization) + GPT4 (inference)",
+        "Gemini 1.5 (itemization) + Llama 3.1 (inference)",
+        "GPT4 (itemization) + Llama 3.1 (inference)",
+        "new line (itemization) + Claude 3.5 (inference)",
+        "new line (itemization) + GPT4 (inference)",
+        "new line (itemization) + Llama 3.1 (inference)"
+    ),
+    0,
 )
+output_format = st.selectbox("Models", ("table", "csv"), 0,)
+output_in_single_table = st.checkbox('Output in single table', value=True)
+remove_empty_columns_from_the_output = st.checkbox('Remove empty columns from the output', value=True)
+output_with_additional_headers = st.checkbox('Output with additional headers', value=False)
+catalogue_dropdown = st.selectbox("Catalog", ("Do not match to the catalog", "Do not match to the catalog"), 0,)
+textarea = st.text_area('Query', '')
+# button = st.button(label="Submit", on_click=aaa(log_area))
+log_area = st.empty()
+
+if 'decomposition_llm_gpt' not in st.session_state:
+    st.session_state.decomposition_llm_gpt = False
+
+
+# def aaa():
+#     global product_catalog_retriever
+#     global log_area
+#     input = textarea
+#     log_area.markdown(st.session_state.decomposition_llm_gpt)
+#     st.session_state.decomposition_llm_gpt = not st.session_state.decomposition_llm_gpt
+
+# st.button(label="Submit", on_click=aaa)
+
+if st.button(label="Submit"):
+    input = textarea
+    log_area.markdown(st.session_state.decomposition_llm_gpt)
+    st.session_state.decomposition_llm_gpt = ChatOpenAI(
+        openai_api_key='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',
+        openai_api_base='https://openrouter.ai/api/v1',
+        model_name="google/gemini-flash-1.5",
+        temperature=0
+      )
